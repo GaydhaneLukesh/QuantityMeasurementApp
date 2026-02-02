b1e2c284ba63f2daec7738247ef7bbf9b92f26e4
GaydhaneLukesh <gaydhanelukesh@gmail.com>
2026-02-02 22:30:00 +0530

    Completed-feature/UC2-InchEquality with Test cases

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -4,13 +4,12 @@
 
 package com.apps.quantitymeasurement;
 
-import org.omg.DynamicAny.DynSequenceOperations;
-
 public class QuantityMeasurementApp {
 
     public static class Feet{
 
         public Feet(double value){
+            if(Double.isNaN(value)) throw new IllegalArgumentException("This exception is thrown because value is not valid(Numeric)");
             this.value = value;
         }
 
@@ -28,12 +27,45 @@ public class QuantityMeasurementApp {
         public int hashCode(){
             return Double.hashCode(value);
         }
+    }
 
+    public static class Inches{
+
+        public Inches(double value){
+            if(Double.isNaN(value)) throw new IllegalArgumentException("This exception is thrown because value is not valid(Numeric)");
+            this.value = value;
+        }
+
+        private final double value;
+
+        @Override
+        public boolean equals(Object obj){
+            if(this == obj) return true;
+            if(obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches)obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+
+        @Override
+        public int hashCode(){
+            return Double.hashCode(value);    //“hashCode must return int, and Double.hashCode(double) is designed exactly for that.”
+        }                                     //Double.hashCode(value) is a static method that: takes a double,returns an int.
+    }
+
+    public static void demonstrateFeetEquality(){
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        System.out.println("Feet value f1, f2 are equal: "+f1.equals(f2));
+    }
+
+    public static void demonstrateInchesEquality(){
+        Inches in1 = new Inches(1.0);
+        Inches in2 = new Inches(1.0);
+        System.out.println("Inches value in1, in1 are equal: "+in1.equals(in2));
     }
 
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-        System.out.println(f1.equals(f2));
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,6 +5,7 @@
 package com.apps.quantitymeasurement;
 
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,35 +13,61 @@ import static org.junit.jupiter.api.Assertions.*;
 public class QuantityMeasurementAppTest {
 
     @Test
+    void testFeetConstructor_WhenNaN_ShouldPass() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Feet(Double.NaN);
+        });
+    }
+
+    @Test
     public void testFeetEquality_SameValue() {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-        assertTrue(f1.equals(f2));
+        assertTrue(new Feet(1.0).equals(new Feet(1.0)));
     }
 
     @Test
     public void testFeetEquality_DifferentValue(){
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(2.0);
-        assertFalse(f1.equals(f2));
+        assertFalse(new Feet(1.0).equals(new Feet(2.0)));
     }
 
     @Test
     public void testFeetEquality_NullComparison() {
-        Feet f1 = new Feet(1.0);
-        assertFalse(f1.equals(null));
+        assertFalse(new Feet(1.0).equals(null));
     }
 
     @Test
     public void testFeetEquality_DifferentClass(){
-        Feet f1 = new Feet(1.0);
-        String str = "1.0";
-        assertFalse(f1.equals(str));
+        assertFalse(new Feet(1.0).equals(new Feet(12)));
     }
 
     @Test
     public void testFeetEquality_SameReference(){
         Feet f1 = new Feet(1.0);
         assertTrue(f1.equals(f1));
+    }
+
+    @Test
+    public void testInchesEquality_SameValue(){
+        assertTrue(new Inches(1.0).equals(new Inches(1.0)));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue(){
+        assertFalse(new Inches(1.0).equals(new Inches(2.0)));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison(){
+        assertFalse(new Inches(1.0).equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass(){
+        assertFalse(new Inches(1.0).equals(new Inches(12)));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference(){
+        Inches in1 = new Inches(1.0);
+        assertTrue(in1.equals(in1));
     }
 }
